714b29ceddf506b3d90f8f1ba836d6b3bb4ceae2
Konstantin <konstantin@theapartment.se>
2018-01-18 00:34:35 +0100

    Fix typo

--- functions.php
+++ functions.php
@@ -193,7 +193,7 @@ add_filter('excerpt_more', 'custom_excerpt_more');
 
 // Enqueue styles
 function enqueue_styles() {
-	wp_enqueue_style('style', get_template_directory_uri() . 'public/css/style.css');
+	wp_enqueue_style('style', get_template_directory_uri() . 'public/css/main.css');
 }
 add_action( 'wp_enqueue_scripts', 'enqueue_styles' );
 
