13340e15473d67c365393703e3590ce562602249
André Ekeberg <andre@pocketsize.se>
2018-01-19 17:53:18 +0100

    Remove BoltsWP JS

--- src/js/app.js
+++ src/js/app.js
@@ -1,11 +1,9 @@
 //import 'babel-polyfill'
 import Bolts from 'bolts';
-import BoltsWP from './bolts-wp'
 
 (() => {
 
 	Bolts.init();
-	BoltsWP.init();
 
 	document.addEventListener('DOMContentLoaded', function() {
 
