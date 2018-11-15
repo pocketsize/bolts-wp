6cd1499138b5e47c20eea04ececf61218f140e35
Kotte <konstantin@studiolye.se>
2018-11-15 15:42:02 +0100

    Adding accordion to components

---
+++ components/accordion.php
@@ -0,0 +1,27 @@
+<?php 
+	/**
+	 * Accordion
+	 * 
+	 * Make sure to include the JS file as well!
+	 * 
+	 * @param array  $items
+	 * @param string $items.title
+	 * @param string $items.content
+	 */
+?>
+
+<ul class="accordion" data-accordion>
+	<?php foreach($items as $item): ?>
+		<li class="accordion-item" data-accordion-item>
+			<div class="accordion-tab" data-accordion-toggle>
+				<h3 class="accordion-title"><?php echo $item['title']; ?></h3>
+			</div>
+
+			<div class="accordion-content" data-auto-height>
+				<div class="accordion-content-inner">
+					<?php echo $item['content']; ?>
+				</div>
+			</div>
+		</li>
+	<?php endforeach; ?>
+</ul>
---
+++ src/js/components/accordion.js
@@ -0,0 +1,43 @@
+/**
+ * Accordion
+ * 
+ * Does the stuff. Uses Elemen.closest() so make sure
+ * to polyfill that if you want to support <Edge
+ */
+
+const accordion = {
+	accordions: null,
+
+	cache() {
+		this.accordions = Array.from(document.querySelectorAll('[data-accordion]'));
+	},
+
+	toggleAccordion(accordionElem) {
+		accordionElem.classList.toggle('is-open');
+	},
+
+	closeAllAccordions(accordionsWrapper) {
+		const elems = Array.from(accordionsWrapper.querySelectorAll('[data-accordion-item]'));
+		elems.forEach(elem => elem.classList.remove('is-open'));
+	},
+
+	init() {
+		this.cache();
+
+		this.accordions.forEach(accordion => {
+			accordion.addEventListener('click', (e) => {
+				if(e.target.closest('[data-accordion-toggle]')) {
+					const accordionItem = e.target.closest('[data-accordion-item]');
+
+					if(!accordionItem.classList.contains('is-open')) {
+						this.closeAllAccordions(accordion);
+					}
+
+					this.toggleAccordion(accordionItem);
+				}
+			});
+		});
+	}
+}
+
+export default accordion;
