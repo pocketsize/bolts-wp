54c527e0f9f6811120e1e7ef3736016d188757dc
Björn Petersson <bjorn@pocketsize.se>
2019-02-20 13:24:39 +0100

    doc block formatting + $type

--- components/common/button.php
+++ components/common/button.php
@@ -1,25 +1,28 @@
 <?php 
-	/**
-	 * Button
-	 * 
-	 * @param string $title
-	 * @param string $target
-	 * @param string $url
-	 * @param string $identifier
-	 * @param string $theme - "default"
-	 */
+/**
+ * Button
+ * 
+ * @param string $title
+ * @param string $target
+ * @param string $url
+ * @param string $identifier
+ * @param string $theme - "default"
+ * @param string $type
+ */
 
 
-	$modifier = !empty($theme) ? 'is-theme-' . $theme : 'is-theme-default';
-	$target   = !empty($target) ? $target : '';
-	$rel      = $target == '_blank' ? 'rel="noopener noreferrer"' : '';
+$modifier = !empty($theme) ? 'is-theme-' . $theme : 'is-theme-default';
+$target   = !empty($target) ? $target : '';
+$rel      = $target == '_blank' ? 'rel="noopener noreferrer"' : '';
+$type     = !empty($type) ? 'type="' . $type . '"' : '';
 
-	$identifier = !empty($identifier) ? 'data-click="' . $identifier . '"' : '';
+// TODO: review this:
+// $identifier = !empty($identifier) ? 'data-click="' . $identifier . '"' : '';
 ?>
 
 <?php if(!empty($title)): ?>
 	<div class="button <?php echo $modifier; ?>" <?php echo $identifier; ?>>
-		<button class="button-inner"><?php echo $title; ?></button>
+		<button class="button-inner" <?php echo $type ?>><?php echo $title; ?></button>
 
 		<?php if(!empty($url)): ?>
 			<a class="button-link" href="<?php echo $url; ?>" target="<?php echo $target; ?>" <?php echo $rel; ?>></a>
