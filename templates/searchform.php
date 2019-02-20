61be914ecf856b91079eb3c88c0b9a057fb059a0
Björn Petersson <bjorn@pocketsize.se>
2019-02-20 13:26:22 +0100

    componentized templates

--- templates/searchform.php
+++
@@ -1,5 +0,0 @@
-<form class="search-form" method="get" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-	<label class="search-form-label" for="s">Search</label>
-	<input class="search-form-input" type="text" name="s" id="s" placeholder="Search" />
-	<button class="search-form-submit" type="submit">Submit</button>
-</form>
