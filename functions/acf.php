7c4cb255188f8a983b669b49e42c7b951f0b9460
Björn Petersson <bjorn@pocketsize.se>
2019-02-21 11:57:50 +0100

    theme options page & 404 page field group

--- functions/acf.php
+++ functions/acf.php
@@ -8,9 +8,9 @@ if ( function_exists('acf_add_options_page') ) {
 	));
 
 	acf_add_options_sub_page(array(
-		'page_title'    => 'General Options',
-		'menu_title'    => 'General',
-		'menu_slug'     => 'general',
+		'page_title'    => 'Theme Options',
+		'menu_title'    => 'Theme',
+		'menu_slug'     => 'theme',
 		'parent_slug'   => $options_page['menu_slug'],
 	));
 
@@ -26,5 +26,54 @@ if ( function_exists('acf_add_options_page') ) {
 		'menu_title'    => 'Footer',
 		'menu_slug'     => 'footer',
 		'parent_slug'   => $options_page['menu_slug'],
+	));
+}
+
+if ( function_exists('acf_add_local_field_group') ) {
+	acf_add_local_field_group(array(
+		'key' => 'group_5c6e7468e7c00',
+		'title' => 'Theme Options',
+		'fields' => array(
+			array(
+				'key' => 'field_5c6e746eb4219',
+				'label' => '404 page',
+				'name' => '404_page',
+				'type' => 'post_object',
+				'instructions' => '',
+				'required' => 0,
+				'conditional_logic' => 0,
+				'wrapper' => array(
+					'width' => '',
+					'class' => '',
+					'id' => '',
+				),
+				'post_type' => array(
+					0 => 'page',
+				),
+				'taxonomy' => array(
+				),
+				'allow_null' => 0,
+				'multiple' => 0,
+				'return_format' => 'id',
+				'ui' => 1,
+			),
+		),
+		'location' => array(
+			array(
+				array(
+					'param' => 'options_page',
+					'operator' => '==',
+					'value' => 'theme',
+				),
+			),
+		),
+		'menu_order' => 0,
+		'position' => 'normal',
+		'style' => 'default',
+		'label_placement' => 'top',
+		'instruction_placement' => 'label',
+		'hide_on_screen' => '',
+		'active' => 1,
+		'description' => '',
 	));
 }
