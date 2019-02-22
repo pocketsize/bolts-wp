2ad0af5d097f1ae7b613705ffddb662948ba231f
Kotte <konstantin@studiolye.se>
2019-02-22 11:15:49 +0100

    Move component testing to page_sandbox.php and filter queries to 404 unwanted endpoints

--- functions/shame.php
+++ functions/shame.php
@@ -6,12 +6,3 @@
 /\__/ / | | || | | || |  | || |___
 \____/\_| |_/\_| |_/\_|  |_/\___*/
 ////////// LIES BELOW ////////////
-
-// function redirect_to_home($query)
-// {
-//     if( is_category() || is_tag() || is_date() || is_author() ) {
-//         global $wp_query;
-//         $wp_query->set_404(); //set to 404 not found page
-//     }
-// }
-// add_action('parse_query', 'redirect_to_home');
