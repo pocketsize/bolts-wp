2e8227b7b5c844089ae1239a486d557e093063d6
Björn Petersson <bjorn@pocketsize.se>
2019-05-02 14:29:17 +0200

    removed helpers call in functions.php

--- functions.php
+++ functions.php
@@ -29,6 +29,5 @@ require_once get_theme_dir() . '/functions/actions.php';
 require_once get_theme_dir() . '/functions/filters.php';
 require_once get_theme_dir() . '/functions/theme.php';
 require_once get_theme_dir() . '/functions/admin.php';
-require_once get_theme_dir() . '/functions/helpers.php';
 require_once get_theme_dir() . '/functions/dev.php';
 require_once get_theme_dir() . '/functions/shame.php';
