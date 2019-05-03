1f6974911a53a14f06f459bd574b2ebf04631e67
Björn Petersson <bjorn@pocketsize.se>
2019-05-03 16:09:34 +0200

    common/button —> forms/button

--- components/forms/button.php
+++ components/forms/button.php
@@ -15,6 +15,6 @@ $modifier   = modifier($theme ?? null, $modifier ?? null);
 
 <button class="button <?php echo $modifier; ?>" <?php echo $attributes; ?>>
 	<?php if (!empty($title)) : ?>
-        <span class="button-inner"><?php echo $title; ?></span>
+        <div class="button-inner"><?php echo $title; ?></div>
 	<?php endif; ?>
 </button>
