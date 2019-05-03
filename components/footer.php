d23a0edc455681d5ec0140eeb79ae3581b2fe579
André Ekeberg <andre@pocketsize.se>
2019-05-03 09:35:07 +0200

    Fix header and footer components

--- components/footer.php
+++ components/footer.php
@@ -3,11 +3,22 @@
  * Footer
  *
  * The main footer of the website
+ * 
+ * @param string $theme
+ * @param string $modifier
+ * @param string $attributes
+ * 
+ * @param string $content
  */
+
+$attributes = attributes($attributes ?? '');
+$modifier   = modifier($theme ?? null, $modifier ?? null);
 ?>
 
-<footer class="footer">
+<footer class="footer <?php echo $modifier; ?>" <?php echo $attributes; ?>>
     <div class="footer-inner">
-        <p>Copyright &copy; <?php bloginfo('name'); ?></p>
+        <?php if (!empty($content)) { ?>
+            <p><?php echo $content; ?></p>
+        <?php } ?>
     </div>
 </footer>
--- components/header.php
+++ components/header.php
@@ -4,20 +4,29 @@
  *
  * The main header of the website
  *
+ * @param string $theme
+ * @param string $modifier
+ * @param string $attributes
+ * 
  * @param array $menu
+ * @param string $home_url
+ * @param string $site_name
  */
+
+$attributes = attributes($attributes ?? '');
+$modifier   = modifier($theme ?? null, $modifier ?? null);
 ?>
 
-<header class="header">
+<header class="header <?php echo $modifier; ?>" <?php echo $attributes; ?>>
     <div class="header-inner">
-        <a href="<?php echo home_url(); ?>" class="header-logo"><?php bloginfo('name'); ?></a>
+        <a href="<?php echo $home_url; ?>" class="header-logo"><?php echo $site_name; ?></a>
 
         <button class="header-menu-toggle" data-menu-toggle>Menu</button>
 
         <div class="header-menu" role="navigation">
             <nav class="main-menu">
                 <?php component('common/menu', [
-                    'menu' => $menu,
+                    'menu'       => $menu,
                     'block_name' => 'main-menu'
                 ]); ?>
             </nav>
