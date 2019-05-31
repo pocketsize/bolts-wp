cd64127cf4268f9f3cc1f550652de9de7f25fe4b
Björn Petersson <bjorn@pocketsize.se>
2019-05-31 11:25:20 +0200

    updated form components

--- components/forms/hidden-input.php
+++ components/forms/hidden-input.php
@@ -4,14 +4,10 @@
  *
  * Simply creates a hidden input.
  *
- * @param string $identifier
- * @param string $name
- * @param string $value
+ * @param string $attributes
  */
 
-$identifier = !empty($identifier) ? 'id="' . $identifier . '"' : '';
-$name       = !empty($name)       ? 'name="' . $name . '"'     : '';
-$value      = !empty($value)      ? 'value="' . $value . '"'   : '';
-?>
+$attributes = attributes($attributes ?? null, ['type' => 'hidden'])
 
-<input type="hidden" <?php echo $identifier; ?> <?php echo $name; ?> <?php echo $value; ?>>
+?>
+<input <?php echo $attributes; ?>>
--- components/forms/radio-button.php
+++ components/forms/radio-button.php
@@ -2,52 +2,30 @@
 /**
  * Radio button
  *
- * Renders a radio buttons. The input is hidden and replaced
- * with a stylable DIV.
- *
- * Automatically uses $title to set $name and $identifier
- * if $name or $identifier are not set.
- *
- * Has a param for validation prepared, but is noot hooked
- * up to any validation engine. Still wainting on finishing
- * Bolts Validate (prototyped in CIKO).
+ * @param string $theme
+ * @param string|array $modifier
+ * @param string|array $attributes
  *
  * @param string $title
- * @param string $name
- * @param string $value
- * @param string $identifier
- * @param bool   $is_checked
- * @param bool   $is_disabled
- * @param bool   $is_required
- * @param string $validate
+ * @param array $input
  *
- * @param string $theme
- * @param string $modifier
  */
 
-$attributes = attributes($attributes ?? '');
 $modifiers  = modifiers($modifiers ?? null, $theme ?? null);
+$attributes = get_attributes($attributes ?? null);
 
-$disabled   = !empty($is_disabled) ? ' disabled' : '';
-$required   = !empty($is_required) ? ' required' : '';
-$identifier = !empty($identifier) ? $identifier : $title;
-$name       = !empty($name) ? $name : $title;
-$value      = !empty($value) ? $value : null;
-$checked    = !empty($is_checked) ? ' checked' : '';
-$validate   = !empty($validate) ? 'data-bolts-validate="' . $validate . '"' : '';
+$input                       = !empty($input) ? $input : [];
+$input['attributes']         = get_attributes($input['attributes'] ?? []);
+$input['attributes']['type'] = !empty($input['attributes']['type']) ? $input['attributes']['type'] : 'radio';
+
+$title = !empty($title) ? $title : '';
+
+$id       = !empty($input['attributes']['id'])       ? $input['attributes']['id']       : false;
+$required = !empty($input['attributes']['required']) ? $input['attributes']['required'] : false;
+
 ?>
-
-<label class="radio-button <?php echo $modifiers; ?>" for="<?php echo $identifier; ?>" <?php echo $attributes; ?>>
-    <input
-        id="<?php echo $identifier; ?>"
-        class="radio-button-input"
-        type="radio"
-        name="<?php echo $name; ?>"
-        value="<?php echo isset($value) ? $value : ''; ?>"
-        <?php echo $disabled; ?>
-        <?php echo $checked; ?>
-        <?php echo $required; ?>
-    />
+<label class="radio-button <?php echo $modifiers; ?>" <?php echo attributes($attributes); ?>>
+    <input class="radio-button-input" <?php echo attributes($input['attributes']); ?> />
     <div class="radio-button-faux-input"></div>
     <span class="radio-button-label"><?php echo $title; ?></span>
 </label>
