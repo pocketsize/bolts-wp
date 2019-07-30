67aba21efe6014b9c0b2bef4d3113de9253742c0
André Ekeberg <andre@pocketsize.se>
2019-07-30 15:45:58 +0200

    Retrieve custom scripts in wp_head and wp_footer

--- functions/acf.php
+++ functions/acf.php
@@ -27,4 +27,23 @@ if (function_exists('acf_add_options_page')) {
         'menu_slug'     => 'footer',
         'parent_slug'   => $options_page['menu_slug'],
     ]);
+
+    if (defined('WP_ENV') && in_array(WP_ENV, ['prod', 'production'])) {
+        $custom_scripts = [
+            'head' => get_field('head_scripts', 'option'),
+            'body' => get_field('body_scripts', 'option')
+        ];
+
+        if (!empty($custom_scripts['head'])) {
+            add_action('wp_head', function() use ($custom_scripts) {
+                echo $custom_scripts['head'];
+            });
+        }
+
+        if (!empty($custom_scripts['body'])) {
+            add_action('wp_footer', function() use ($custom_scripts) {
+                echo $custom_scripts['body'];
+            });
+        }
+    }
 }
