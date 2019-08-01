21a94739557d15e315efddc0f9351679309e4dca
Björn Petersson <bjorn@pocketsize.se>
2019-08-01 15:45:53 +0200

    nullcheck menu items

--- components/common/menu.php
+++ components/common/menu.php
@@ -9,15 +9,15 @@
  * @param string $items
  *
  */
-$items = !empty($items) ? $items : false;
-
 ?>
 <nav class="menu <?php echo modifiers($modifiers ?? null); ?>" <?php echo attributes($attributes ?? null); ?>>
-    <ul class="menu-items">
-        <?php foreach ( $items as $item ) { ?>
-            <li class="menu-item <?php modifiers($item['modifiers'] ?? null, false); ?>" <?php echo attributes($item['attributes'] ?? ['data-bolts-selector' => 'menu-item']); ?>>
-                <?php layout_items($item['items']); ?>
-            </li>
-        <?php } ?>
-    </ul>
+	<?php if (!empty($items)) : ?>
+	    <ul class="menu-items">
+	        <?php foreach ( $items as $item ) { ?>
+	            <li class="menu-item <?php modifiers($item['modifiers'] ?? null, false); ?>" <?php echo attributes($item['attributes'] ?? ['data-bolts-selector' => 'menu-item']); ?>>
+	                <?php layout_items($item['items']); ?>
+	            </li>
+	        <?php } ?>
+	    </ul>
+	<?php endif; ?>
 </nav>
